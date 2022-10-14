4649475661e9efac9114ad6059f834fb03db4ce5
rozhiar <rozyar.313175000@uhd.edu.iq>
2022-10-14 23:48:48 +0300

    Completed assiment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft(){
+    const leftNumber=dodger.style.left.replace('px','');
+    const left=parseInt(leftNumber,10);
+    if(left>0){
+    
+        dodger.style.left = `${left -1}px`;
+    }
+}
+
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+
+});
+function moveDodgerRight() {
+
+    const leftNumber = dodger.style.left.replace('px','');
+    const left= parseInt(leftNumber,10);
+    dodger.style.width='40px';
+    const wid= dodger.style.width.replace('px','');
+    const widchange=parseInt(wid,10);
+    if (widchange===40 && left<360 ) {
+
+        dodger.style.left = `${left +1}px`;
+        console.log(widchange)
+
+
+    }
+
+
+}
+
+document.addEventListener("keydown", function (e) {
+
+    if (e.key === "ArrowRight") {
+    moveDodgerRight();
+    
+  }
+});
+
